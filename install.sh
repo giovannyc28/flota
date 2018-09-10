be153807d98f16d876579fb1a168f0dd45d460ca
giovanny <giovanny@localhost.localdomain>
2018-09-09 22:09:46 -0500

    justes en README y en instalador

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
 docker volume create --name=db_data
 docker-compose build web
 docker-compose up -d
-docker exec -i -t flota_web_1 /bin/bash
+docker exec -i flota_web_1 python manage.py migrate auth
+docker exec -i flota_web_1 python manage.py migrate
+docker exec -i -t flota_web_1 python manage.py createsuperuser
+
+		
